585c9eafec360dfad90b53a5b09fe1c270072805
Bob Huang <hzy.sync@gmail.com>
2019-04-07 20:56:59 +1000

    merge GoatAST.hs and GoatParser.hs from origin/wentao into src/

--- src/GoatAST.hs
+++ src/GoatAST.hs
@@ -4,17 +4,17 @@ module GoatAST where
 -- Specification of an AST for Goat
 -----------------------------------
 
---Data Scale s where
---    Scale :: Num s => s -> s -> Scale s
-
 type Ident = String
+
 data ArraySize
-    = OneDimen Int
-    | Matrix Int Int -- Matrix [Int, Int]
+  = OneDimen Int
+  | Matrix Int Int 
+    deriving (Show, Eq)
 
 data ArrayIndex
-    = OneDimenIndex Expr
-    | MatrixIndex Expr Expr
+  = OneDimenIndex Expr
+  | MatrixIndex Expr Expr
+    deriving (Show, Eq)
 
 data BaseType
     = BoolType | IntType | FloatType
@@ -28,33 +28,16 @@ data Lvalue
 data Binop
     = Op_or | Op_and | Op_add | Op_mul | Op_sub | Op_div | Op_eq |     
     Op_ne | Op_gt | Op_gte | Op_lt | Op_lte
-    deriving (Show, Enum)
-
-instance Show Binop where
-    show Op_or  = "||"
-    show Op_and = "&&"
-    show Op_add = "+"
-    show Op_sub = "-"
-    show Op_div = "/"
-    show Op_mul = "*"
-    show Op_eq  = "="
-    show Op_ne  = "!="
-    show Op_gt  = ">"
-    show Op_gte = ">="
-    show Op_lt  = "<"
-    show Op_lte = "<="
+    deriving (Show, Enum, Eq)
 
 data UnaryOp
     = Op_uneg | Op_umin
-    deriving (Show, Enum)
-
-instance Show UnaryOp where
-    show Op_uneg = "!"
-    show Op_umin = "-"
+    deriving (Show, Enum, Eq)
 
 data Expr
     = BoolConst Bool
     | IntConst Int
+    | FloatConst Float
     | StrConst String
     | Id Ident
     | Array Ident ArrayIndex
@@ -83,11 +66,33 @@ data Proc
     = Proc Ident [Param] [Decl] [Stmt]
     deriving (Show, Eq)
 
-data ParamType = Ref | Val
+data ParamType 
+  = Ref | Val
     deriving (Show, Eq)
     
 data Param
-    = Param ParamType BaseType Ident
+  = Param ParamType BaseType Ident
+    deriving (Show, Eq)
 
 data GoatProgram
-    = Program [Proc]
+  = Program [Proc]
+    deriving (Show, Eq)
+
+
+instance Show Binop where
+    show Op_or  = "||"
+    show Op_and = "&&"
+    show Op_add = "+"
+    show Op_sub = "-"
+    show Op_div = "/"
+    show Op_mul = "*"
+    show Op_eq  = "="
+    show Op_ne  = "!="
+    show Op_gt  = ">"
+    show Op_gte = ">="
+    show Op_lt  = "<"
+    show Op_lte = "<="
+
+instance Show UnaryOp where
+    show Op_uneg = "!"
+    show Op_umin = "-"

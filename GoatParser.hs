dabbb9c33884fb14c2e04d270a9bc4135a119efa
Wentao Liu <liuwnt@gmail.com>
2019-04-09 19:19:23 +1000

    less backtracking

--- GoatParser.hs
+++ GoatParser.hs
@@ -37,6 +37,7 @@ parens     = Q.parens lexer
 squares    = Q.squares lexer
 reserved   = Q.reserved lexer
 reservedOp = Q.reservedOp lexer
+naturalOrFloat = Q.naturalOrFloat lexer
 
 myReserved, myOpnames :: [String]
 
@@ -98,26 +99,14 @@ pProgBody
 pDecl :: Parser Decl
 pDecl
   = do
-    try pBaseDecl <|>  pArrayDecl
-
-pBaseDecl :: Parser Decl
-pBaseDecl
-  = do
     basetype <- pBaseType
     ident <- identifier
+    size <- optionMaybe $ squares pArraySize
     whiteSpace
     semi
-    return (BaseDecl ident basetype)
-
-pArrayDecl :: Parser Decl
-pArrayDecl 
-  = do
-    basetype <- pBaseType
-    ident <- identifier
-    size <- squares pArraySize
-    whiteSpace
-    semi
-    return (ArrayDecl ident size basetype)
+    case size of 
+      Nothing -> return (BaseDecl ident basetype)
+      Just s -> return (ArrayDecl ident s basetype)
 
 pArraySize :: Parser ArraySize
 pArraySize
@@ -213,12 +202,12 @@ pWhile
 --  are left-associative.
 -----------------------------------------------------------------
 
-pExp, pIntConst, pFloatConst, pIdent, pString, pBool :: Parser Expr
+pExp, pNum, pIdent, pString, pBool :: Parser Expr
 
 pExp = buildExpressionParser table pFac 
         <?> "expression"
 
-pFac = choice [parens pExp, pIntConst, pFloatConst, pIdent, pString, pBool]
+pFac = choice [parens pExp, pNum, pIdent, pString, pBool]
 
 table = [ [ prefix "-" (UnaryExpr Op_umin) ]
         , [ binary "*" (BinExpr Op_mul), binary "/" (BinExpr Op_div) ] 
@@ -249,19 +238,14 @@ pBool
     <|>
     do { reserved "false"; return (BoolConst False) }
 
-pIntConst
+pNum
   = do
-      n <- natural <?> ""
-      return (IntConst (fromInteger n :: Int))
+        n <- naturalOrFloat;
+        case n of 
+          Left i -> return (IntConst (fromInteger i :: Int))
+          Right f -> return (FloatConst (realToFrac f :: Float))
     <?>
     "number"
-
-pFloatConst
-  = do
-      n <- float <?> ""
-      return (FloatConst (realToFrac n :: Float))
-    <?>
-    "float"
 
 pIdent 
   = do
@@ -274,23 +258,13 @@ pIdent
 pLvalue :: Parser Lvalue
 pLvalue
   = do 
-    try pLarray <|> pLatom
+    ident <- identifier
+    aindex <- optionMaybe $ squares pArrayIndex
+    case aindex of 
+      Nothing -> return (LId ident)
+      Just i -> return (LArray ident i)
     <?>
     "lvalue"
-
-pLatom :: Parser Lvalue
-pLatom 
-  = do
-    ident <- identifier
-    return (LId ident)
-
-pLarray :: Parser Lvalue
-pLarray 
-  = do
-      ident <- identifier
-      aindex <- squares pArrayIndex
-      return (LArray ident aindex)
-  
 
 pArrayIndex :: Parser ArrayIndex
 pArrayIndex

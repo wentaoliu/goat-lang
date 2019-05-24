379f8ba70dea4e316e4a7c208f4a68076fffa387
Bob Huang <hzy.sync@gmail.com>
2019-05-24 20:45:01 +1000

    bug fix: return BoolType for relops

--- GoatCodegen.hs
+++ GoatCodegen.hs
@@ -347,13 +347,14 @@ cgExpression (UnaryMinus _ expr) = do
         BoolType -> error $ "expected integer or real, found boolean"
     writeInstruction func [showReg reg, showReg reg]
     return (reg, typ)
+
 -- Not Pos Expr
 cgExpression (Not _ expr) = do
     (reg, typ) <- cgExpression expr
     case typ of
         BoolType -> writeInstruction "not" [showReg reg, showReg reg]
         otherwise -> error $ "expected bool, found " ++ show typ
-    return (reg, typ)
+    return (reg, BoolType)
 
 -- And | Or
 cgExpression (And _ expr1 expr2) = do
@@ -361,14 +362,14 @@ cgExpression (And _ expr1 expr2) = do
     (reg2, typ2) <- cgExpression expr2
     cgPrepareLogical reg1 reg2
     writeInstruction "and" [showReg reg1, showReg reg1, showReg reg2]
-    return (reg1, typ1) -- (typ1 == typ2 == BoolType)
+    return (reg1, BoolType) -- (typ1 == typ2 == BoolType)
 
 cgExpression (Or _ expr1 expr2) = do
     (reg1, typ1) <- cgExpression expr1
     (reg2, typ2) <- cgExpression expr2
     cgPrepareLogical reg1 reg2
     writeInstruction "or" [showReg reg1, showReg reg1, showReg reg2]
-    return (reg1, typ1) -- (typ1 == typ2 == BoolType)
+    return (reg1, BoolType) -- (typ1 == typ2 == BoolType)
 
 -- Rel Pos Relop Expr Expr
 cgExpression (Rel _ relop expr1 expr2) = do
@@ -389,7 +390,8 @@ cgExpression (Rel _ relop expr1 expr2) = do
             OpType FloatType -> "real"
     writeInstruction (relopInstruction ++ relopType)
                      [showReg reg1, showReg reg1, showReg reg2]
-    return (reg1, fromOpType optype)
+    -- return (reg1, fromOpType optype)
+    return (reg1, BoolType)
 
 -- Id Pos Ident
 -- getVariable :: String -> Codegen (Bool, BaseType, Int)
